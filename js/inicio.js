275e80bf768e3326284de0ef4bc55a08221e61c4
AmarantaC <amarcitla@gmail.com>
2022-10-26 18:16:30 -0500

    Agregando pagina

---
+++ js/inicio.js
@@ -0,0 +1,91 @@
+
+/*----- Rutas Json ------*/
+
+const tarot = "json/tarot.json";
+
+/*------Inicio-----*/
+
+const seccion = document.getElementById("seccion");
+const inicio = document.createElement("div");
+inicio.innerHTML=`
+<div class="caja imagenTarot" id="imagenTarot">
+    <img src="img/inicio1.png">
+</div>
+<div class="caja texto">
+    <h1>Elije una carta para descubrir el mensaje del tarot</h1>
+<div class="caja cartas">
+    <div class="carta">
+        <img src="img/carta.jpg"> 
+    </div>
+    <div class="carta">
+        <img src="img/carta.jpg"> 
+    </div>
+    <div class="carta">
+        <img src="img/carta.jpg"> 
+    </div>
+</div>                        
+</div>                                                           
+`
+inicio.classList.add("tarot");
+seccion.appendChild(inicio);
+
+const cartasTarot = document.getElementsByClassName("carta");                        
+                
+fetch(tarot)
+    .then(respuesta => respuesta.json())
+    .then(carta => {        
+
+        for(let i = 0; i < cartasTarot.length; i++){
+            cartasTarot[i].addEventListener('click', leerCarta);  
+
+        }                       
+
+        function leerCarta(){
+            let numSuerte = (Math.round(Math.random() * 22));
+            console.log(numSuerte);
+            let cartaSuerte = carta.find(carta=> carta.id === numSuerte);  
+            console.log(cartaSuerte);
+
+            let cartaElegida = document.createElement("div")
+            cartaElegida.innerHTML = `
+            <div class="caja imagenCarta" id="imagenCarta">
+                <img src=${cartaSuerte.img}>
+            </div>
+            <div class="caja texto">
+                <h1><strong>${cartaSuerte.nombre}</strong></h1>
+                <p>${cartaSuerte.mensaje}</p>  
+            </div>            
+            <div class="botones" id="botonesInicio">
+                <div class="boton" id="salir">Salir</div>    
+            </div>                              
+            `
+            seccion.removeChild(inicio);
+            cartaElegida.classList.add("cartaElegida");                
+            seccion.appendChild(cartaElegida);
+
+            const salir = document.getElementById("salir");
+            salir.addEventListener("click", ()=>{
+                seccion.appendChild(fin);
+                seccion.removeChild(cartaElegida);
+            })
+        }                                  
+})
+.catch(error => console.log(error))
+.finally(() => console.log("Proceso Finalizado"));
+
+const fin = document.createElement("div");
+fin.innerHTML=`
+<div class="caja imagenFin">
+    <img src="img/inicio1.png">
+</div>
+<div class="caja texto">
+    <h1>Hasta pronto</h1>
+    <p>Hecho con ❤ por <a href="https://amarantac.github.io/portafolio/">Amaranta</a> </p> 
+                         
+</div> 
+<p class="creditos">Ilustraciones por <a href="https://www.cocorrina.com/">cocorrina</a> </p>                                                          
+`
+fin.classList.add("tarot");
+
+
+       
